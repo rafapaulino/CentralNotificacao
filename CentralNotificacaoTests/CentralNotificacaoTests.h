2207c7d8bd5654883f22c0aabe72a3623f4099a1
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-24 11:42:17 -0300

    Initial Commit

---
+++ CentralNotificacaoTests/CentralNotificacaoTests.h
@@ -0,0 +1,13 @@
+//
+//  CentralNotificacaoTests.h
+//  CentralNotificacaoTests
+//
+//  Created by Rafael Brigagão Paulino on 24/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CentralNotificacaoTests : SenTestCase
+
+@end
---
+++ CentralNotificacaoTests/CentralNotificacaoTests.m
@@ -0,0 +1,32 @@
+//
+//  CentralNotificacaoTests.m
+//  CentralNotificacaoTests
+//
+//  Created by Rafael Brigagão Paulino on 24/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "CentralNotificacaoTests.h"
+
+@implementation CentralNotificacaoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CentralNotificacaoTests");
+}
+
+@end
